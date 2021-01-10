6ab5a43505521f3ba5a7ee5589c40cbb0fa1f198
Artem <artemiyo.dev@gmail.com>
2021-01-10 12:27:59 +0300

    refactor: create custom render

--- src/__tests__/react-router-01.js
+++ src/__tests__/react-router-01.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import userEvent from '@testing-library/user-event'
-import {render, screen} from '@testing-library/react'
+import {render as rtlRender, screen} from '@testing-library/react'
 import {Main} from '../main'
 
+function render(ui, {route = '/', ...renderOptions} = {}) {
+  window.history.pushState({}, 'Test Page', route)
+
+  function Wrapper({children}) {
+    return <BrowserRouter>{children}</BrowserRouter>
+  }
+
+  return rtlRender(ui, {
+    wrapper: Wrapper,
+    ...renderOptions,
+  })
+}
+
+function textContentAssertion(textContent) {
+  expect(screen.getByRole('heading')).toHaveTextContent(textContent)
+}
+
 test('main renders about and home and I can navigate to those pages', () => {
-  window.history.pushState({}, 'Test Page', '/')
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  )
-  expect(screen.getByRole('heading')).toHaveTextContent(/home/i)
+  render(<Main />)
+  textContentAssertion(/home/i)
   userEvent.click(screen.getByText(/about/i))
-  expect(screen.getByRole('heading')).toHaveTextContent(/about/i)
+  textContentAssertion(/about/i)
 })
 
 test('landing on a bad page shows no match component', () => {
-  window.history.pushState({}, 'Test page', '/something-that-does-not-match')
-  render(
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>,
-  )
-  expect(screen.getByRole('heading')).toHaveTextContent(/404/i)
+  render(<Main />, {route: '/something-that-does-not-match'})
+  textContentAssertion(/404/i)
 })
